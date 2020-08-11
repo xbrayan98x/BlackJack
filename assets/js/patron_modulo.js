5b6c6578e0aebea1097c4658fe4af997d29c5f12
user <brayanrojas113@gmail.com>
2020-08-11 11:43:50 -0500

    commit inicial

---
+++ assets/js/patron_modulo.js
@@ -0,0 +1,21 @@
+
+// sintaxis del patron modulo.
+
+() =>{
+  /*
+    se crea un nuevo scope entre el cuerpo de la funcion
+    el cuál no tiene una refrencia por nombre, por ende
+    no se puede llamar el objeto directamente.
+
+    - leer sobre 'use strict'
+  */
+  'use strict'
+
+  // # code
+
+}
+
+// funcion anonima auto invocada.
+(() =>{
+  // #code
+})();
